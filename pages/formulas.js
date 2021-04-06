b41ef759f76bac3947d24d5066066c2061dd9cb9
databenti <databenti@gmail.com>
2021-04-06 12:26:46 -0300

    update

--- pages/formulas.js
+++ pages/formulas.js
@@ -36,7 +36,7 @@ export default function Home({ items }) {
               <h2 className={styles.titlecard}>{item.title}</h2>
               <p><span className={styles.price}>{currencyFormat(item.price)}</span></p><br />
               <span>Ultima atualização: </span>
-              <p className={styles.updatedata}><span>{moment(item.updated).format('ll')}</span></p>
+              <p className={styles.updatedata}><span className={styles.data}>{moment(item.updated).format('ll')}</span></p>
         
             </div>
           ))}
